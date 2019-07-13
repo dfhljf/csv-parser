df5ce2db761ea178e813ca175d0dda1f9af8e325
hui.jiang <jianghui19940917@hotmail.com>
2019-07-13 20:54:40 +0800

    fix bug

--- include/internal/data_type.h
+++ include/internal/data_type.h
@@ -43,7 +43,7 @@ namespace csv {
 
         /* Compute 10 to the power of n */
         template<typename T>
-        const long double pow10(const T& n) {
+        long double pow10(const T& n) {
             long double multiplicand = n > 0 ? 10 : 0.1,
                 ret = 1;
             T iterations = n > 0 ? n : -n;
